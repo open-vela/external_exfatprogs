975ca7c215165f784ae58f0bd5b61c5ec4945140
openvela-robot <openvela-robot@xiaomi.com>
2021-02-23 23:00:28 +0800

    mkfs: add --pack-bitmap option to control bitmap packing
    Also document the new option in the manpage.
    
    Signed-off-by: Matt Whitlock <exfatprogs@mattwhitlock.name>

--- dump/dump.c
+++ dump/dump.c
@@ -148,7 +148,7 @@ int exfat_show_ondisk_all_info(struct exfat_blk_dev *bd)
 		goto free_entry;
 	}
 
-	volume_label = exfat_conv_volume_label(&ed[0]);
+	volume_label = exfat_conv_volume_serial(&ed[0]);
 	if (!volume_label) {
 		ret = -EINVAL;
 		goto free_entry;
@@ -195,7 +195,6 @@ int exfat_show_ondisk_all_info(struct exfat_blk_dev *bd)
 	exfat_info("Cluster size:  \t\t\t\t%u\n", bd->cluster_size);
 	exfat_info("Total Clusters: \t\t\t%u\n", total_clus);
 	exfat_info("Free Clusters: \t\t\t\t%u\n", total_clus-used_clus);
-	ret = 0;
 
 	free(bitmap);
 
--- lib/libexfat.c
+++ lib/libexfat.c
@@ -6,6 +6,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/ioctl.h>
+#include <sys/sysmacros.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
@@ -144,6 +145,8 @@ int exfat_get_blk_dev_info(struct exfat_user_input *ui,
 {
 	int fd, ret = -1;
 	off_t blk_dev_size;
+	struct stat st;
+	unsigned long long blk_dev_offset = 0;
 
 	fd = open(ui->dev_name, ui->writeable ? O_RDWR|O_EXCL : O_RDONLY);
 	if (fd < 0) {
@@ -160,7 +163,24 @@ int exfat_get_blk_dev_info(struct exfat_user_input *ui,
 		goto out;
 	}
 
+	if (fstat(fd, &st) == 0 && S_ISBLK(st.st_mode)) {
+		char pathname[43];
+		FILE *fp;
+		snprintf(pathname, sizeof pathname, "/sys/dev/block/%u:%u/start",
+			major(st.st_rdev), minor(st.st_rdev));
+		if ((fp = fopen(pathname, "r")) != NULL) {
+			if (fscanf(fp, "%llu", &blk_dev_offset) == 1)
+				/*
+				 * Linux kernel always reports partition offset
+				 * in 512-byte units, regardless of sector size
+				 */
+				blk_dev_offset <<= 9;
+			fclose(fp);
+		}
+	}
+
 	bd->dev_fd = fd;
+	bd->offset = blk_dev_offset;
 	bd->size = blk_dev_size;
 	if (!ui->cluster_size)
 		exfat_set_default_cluster_size(bd, ui);
@@ -175,7 +195,8 @@ int exfat_get_blk_dev_info(struct exfat_user_input *ui,
 	bd->num_clusters = blk_dev_size / ui->cluster_size;
 
 	exfat_debug("Block device name : %s\n", ui->dev_name);
-	exfat_debug("Block device size : %lld\n", bd->size);
+	exfat_debug("Block device offset : %llu\n", bd->offset);
+	exfat_debug("Block device size : %llu\n", bd->size);
 	exfat_debug("Block sector size : %u\n", bd->sector_size);
 	exfat_debug("Number of the sectors : %llu\n",
 		bd->num_sectors);
@@ -356,7 +377,7 @@ off_t exfat_get_root_entry_offset(struct exfat_blk_dev *bd)
 	return root_clu_off;
 }
 
-char *exfat_conv_volume_label(struct exfat_dentry *vol_entry)
+char *exfat_conv_volume_serial(struct exfat_dentry *vol_entry)
 {
 	char *volume_label;
 	__le16 disk_label[VOLUME_LABEL_MAX_LEN];
@@ -370,7 +391,6 @@ char *exfat_conv_volume_label(struct exfat_dentry *vol_entry)
 	if (exfat_utf16_dec(disk_label, vol_entry->vol_char_cnt*2,
 		volume_label, VOLUME_LABEL_BUFFER_SIZE) < 0) {
 		exfat_err("failed to decode volume label\n");
-		free(volume_label);
 		return NULL;
 	}
 
@@ -393,20 +413,16 @@ int exfat_show_volume_label(struct exfat_blk_dev *bd, off_t root_clu_off)
 		sizeof(struct exfat_dentry), root_clu_off);
 	if (nbytes != sizeof(struct exfat_dentry)) {
 		exfat_err("volume entry read failed: %d\n", errno);
-		free(vol_entry);
 		return -1;
 	}
 
-	volume_label = exfat_conv_volume_label(vol_entry);
-	if (!volume_label) {
-		free(vol_entry);
+	volume_label = exfat_conv_volume_serial(vol_entry);
+	if (!volume_label)
 		return -EINVAL;
-	}
 
 	exfat_info("label: %s\n", volume_label);
 
 	free(volume_label);
-	free(vol_entry);
 	return 0;
 }
 
