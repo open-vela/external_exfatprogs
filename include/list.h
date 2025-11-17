a689f6105abb11259bb53a0a3a8c9c6d2b0554f0
Bowen Wang <wangbowen6@xiaomi.com>
2025-11-17 16:31:29 +0800

    exfatprogs: avoid container_of redefine error
    Signed-off-by: Bowen Wang <wangbowen6@xiaomi.com>

--- include/list.h
+++ include/list.h
@@ -7,10 +7,12 @@
 #define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
 #endif
 
+#ifndef container_of
 #define container_of(ptr, type, member) ({			\
 	const typeof(((type *)0)->member) * __mptr = (ptr);	\
 	(type *)((char *)__mptr - offsetof(type, member));	\
 	})
+#endif
 
 /*
  * These are non-NULL pointers that will result in page faults
